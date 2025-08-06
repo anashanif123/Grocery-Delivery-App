1972225db91c0b6a84be9f5173dedc671620332f
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-08-06 13:13:40 +0500

    (branch,category,counter,order,products,user)  All models have been done and create index.js for all the models can axcees in this file

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+import Order from "./order.js"
+import Counter from "./counter.js"
+import Category from "./category.js"
+import Product from "./products"
+import Branch from "./branch.js"
+import { Customer,DeliveryPartner,Admin } from "./user"
+
+export{
+  Order,
+  Counter,
+  Customer,
+  DeliveryPartner,
+  Admin,
+  Category,
+  Product,
+  Branch
+}
